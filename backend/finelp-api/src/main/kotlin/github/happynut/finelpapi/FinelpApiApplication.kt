007def1bf1536561297f75f80f27ba55303036f0
happy-nut <happynut.dev@gmail.com>
2021-01-24 22:45:01 +0900

    Init backend project with Spring framework and Kotlin

---
+++ backend/finelp-api/src/main/kotlin/github/happynut/finelpapi/FinelpApiApplication.kt
@@ -0,0 +1,11 @@
+package github.happynut.finelpapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinelpApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinelpApiApplication>(*args)
+}
---
+++ backend/finelp-api/src/test/kotlin/github/happynut/finelpapi/FinelpApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package github.happynut.finelpapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FinelpApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
